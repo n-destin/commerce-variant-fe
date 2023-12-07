cad7ff398ef198f4e6d8ebb072abd2f7a8955307
Eric Niyonkuru <eric.niyonkuru@pesachoice.com>
2023-12-07 22:34:56 +0200

    fix chart input message

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { createNewUser } from "../../apis/user";
 import toast from "react-hot-toast";
+import { SyncLoader } from "react-spinners";
 
 const Signup = () => {
   const {
@@ -90,7 +91,11 @@ const Signup = () => {
                   type='submit'
                   className='w-full text-white bg-green-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
                 >
-                  Create an account
+                  {newUserMutation.isPending ? (
+                    <SyncLoader size={8} color='#fff' />
+                  ) : (
+                    "Create an account"
+                  )}
                 </button>
               </div>
               <p className='text-sm font-light text-gray-500 flex'>
