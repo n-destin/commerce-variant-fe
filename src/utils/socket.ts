92decb355bc418bc4c5b68b2647cc636dec9b5b0
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2023-12-25 18:54:25 -0500

    socket conflict merge

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,3 @@
 import { io } from "socket.io-client";
-<<<<<<< HEAD
 export const socket = io("https://collegecommerceapi.onrender.com");
-=======
-export const socket = io("https://collegemarket.onrender.com");
->>>>>>> 4eeef3c40ae8359ece91def1377437a8edce2f9e
+
