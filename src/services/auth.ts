208d96baeed8ea0d73a131219c6f34153729b20d
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2024-01-12 21:57:38 -0500

    login update

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,7 +4,7 @@ import httpClient from "../utils/httpClient";
 export const getProfile = async (token: string): Promise<IUser> => {
   return (
     await httpClient.get("/users/profile", {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: {Authorization: `Bearer ${token}` },
     })
   ).data;
 };
